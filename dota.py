129069229805bfae3b4407b17aed5140e59fc9b0
Callmegod <125900081+Callmeg0d@users.noreply.github.com>
2024-06-20 13:32:40 +0300

    add pydantic

--- dota.py
+++ dota.py
@@ -1,16 +1,28 @@
 import requests
+from pydantic import BaseModel
 import json
 from http import HTTPStatus
 
 API_OPENDOTA = "https://api.opendota.com"
 
 url = "https://api.opendota.com/api/heroes"
+
+class HeroData(BaseModel):
+    id: int
+    name: str
+    localized_name: str
+    primary_attr: str
+    attack_type: str
+
 try:
     response = requests.get(url)
     response.raise_for_status()
     heroes_data = response.json()
-except requests.exceptions.RequestException:
-    print(f"Сетевая ошибка:")
+    validated_data = [HeroData(**hero) for hero in heroes_data] #все ключи-значения
+except requests.exceptions.RequestException as e:
+    print(f"Сетевая ошибка: {e}")
+except ValueError as e:
+    print(f"Ошибка при обработке JSON: {e}")
 
 def name_by_id(some_info_about_hero):
     hero_id = some_info_about_hero["hero_id"]
@@ -18,20 +30,28 @@ def name_by_id(some_info_about_hero):
         if heroes_data[i]["id"] == hero_id:
             return(heroes_data[i]["localized_name"])
 
+class HeroMatchup(BaseModel):
+    hero_id: int
+    games_played: int
+    wins: int
+    win_rate: float
+
 def get_hero_versus(hero_id, hero_data):
     url = f"https://api.opendota.com/api/heroes/{hero_id}/matchups"
     try:
         response = requests.get(url)
+        response.raise_for_status()
         hero_versus_data = response.json()
         versus_heroes = []
         for hero in hero_versus_data:
-            if hero['games_played'] >= 5 :
-                versus_heroes.append({
-                    "hero_id": hero["hero_id"],
-                    "games_played": hero["games_played"],
-                    "wins": hero["wins"],
-                    "win_rate": round(hero["wins"] / hero["games_played"] * 100, 2)
-                })
+            matchup_info = HeroMatchup(
+                hero_id=hero["hero_id"],
+                games_played=hero["games_played"],
+                wins=hero["wins"],
+                win_rate=round(hero["wins"] / hero["games_played"] * 100, 2)
+            )
+            if matchup_info.games_played >= 5:
+                versus_heroes.append(matchup_info.dict())
         sorted_versus_heroes = sorted(versus_heroes, key=lambda x: x["win_rate"], reverse=True)
         print("Герой наиболее силён против:")
         print(sorted_versus_heroes[0])
@@ -44,9 +64,12 @@ def get_hero_versus(hero_id, hero_data):
         print(sorted_versus_heroes[-2])
         print(name_by_id(sorted_versus_heroes[-2]))
         print()
-    except requests.exceptions.RequestException:
-        print(f"Сетевая ошибка:")
-        return None
+    except requests.exceptions.RequestException as e:
+        print(f"Сетевая ошибка: {e}")
+        return []
+    except ValueError as e:
+        print(f"Ошибка при обработке JSON: {e}")
+        return []
 
 def hero_by_name(hero_name, hero_data):
     for hero in hero_data:
@@ -63,9 +86,9 @@ def print_hero_info(hero):
         print()
 
 def main():
-
+    print("Программа выдаёт 2 лучших и худших героев против введённого, их статистику матчей, а также некоторые характеристики введённого героя.")
     while True:
-        hero_name = input("Введите имя героя (для выхода введите 'exit'): ")
+        hero_name = input("Введите имя героя, чтобы узнать его статистику и характеристики (для выхода введите 'exit'): ")
         if hero_name.lower() == "exit":
             break
         hero = hero_by_name(hero_name, heroes_data)
