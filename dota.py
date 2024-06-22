4bb4a304afd2c1c53b8c5d990e9986234c0c0fb5
Callmegod <125900081+Callmeg0d@users.noreply.github.com>
2024-06-22 11:46:59 +0300

    Update dota.py

--- dota.py
+++ dota.py
@@ -1,28 +1,23 @@
 import requests
-from pydantic import BaseModel
 import json
 from http import HTTPStatus
 
-API_OPENDOTA = "https://api.opendota.com"
+OPENDOTA_API = "https://api.opendota.com"
 
-url = "https://api.opendota.com/api/heroes"
-
-class HeroData(BaseModel):
-    id: int
-    name: str
-    localized_name: str
-    primary_attr: str
-    attack_type: str
-
-try:
-    response = requests.get(url)
-    response.raise_for_status()
-    heroes_data = response.json()
-    validated_data = [HeroData(**hero) for hero in heroes_data] #все ключи-значения
-except requests.exceptions.RequestException as e:
-    print(f"Сетевая ошибка: {e}")
-except ValueError as e:
-    print(f"Ошибка при обработке JSON: {e}")
+def hero_info():
+    url = f"{OPENDOTA_API}/api/heroes"
+    global heroes_data
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        heroes_data = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Сетевая ошибка: {e}")
+    try:
+        validated_data = [HeroData(**hero) for hero in heroes_data]
+    except ValueError as e:
+        print(f"Ошибка при обработке JSON: {e}")
+    return heroes_data
 
 def name_by_id(some_info_about_hero):
     hero_id = some_info_about_hero["hero_id"]
@@ -30,14 +25,8 @@ def name_by_id(some_info_about_hero):
         if heroes_data[i]["id"] == hero_id:
             return(heroes_data[i]["localized_name"])
 
-class HeroMatchup(BaseModel):
-    hero_id: int
-    games_played: int
-    wins: int
-    win_rate: float
-
 def get_hero_versus(hero_id, hero_data):
-    url = f"https://api.opendota.com/api/heroes/{hero_id}/matchups"
+    url = f"{OPENDOTA_API}/api/heroes/{hero_id}/matchups"
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -54,15 +43,11 @@ def get_hero_versus(hero_id, hero_data):
                 versus_heroes.append(matchup_info.dict())
         sorted_versus_heroes = sorted(versus_heroes, key=lambda x: x["win_rate"], reverse=True)
         print("Герой наиболее силён против:")
-        print(sorted_versus_heroes[0])
-        print(name_by_id(sorted_versus_heroes[0]))
-        print(sorted_versus_heroes[1])
-        print(name_by_id(sorted_versus_heroes[1]))
+        for i in range(2):
+            print(sorted_versus_heroes[i], (name_by_id(sorted_versus_heroes[i])))
         print("Герой наиболее слаб против:")
-        print(sorted_versus_heroes[-1])
-        print(name_by_id(sorted_versus_heroes[-1]))
-        print(sorted_versus_heroes[-2])
-        print(name_by_id(sorted_versus_heroes[-2]))
+        for i in range(2):
+            print(sorted_versus_heroes[len(sorted_versus_heroes) - i - 1], (name_by_id(sorted_versus_heroes[len(sorted_versus_heroes) - i - 1])))
         print()
     except requests.exceptions.RequestException as e:
         print(f"Сетевая ошибка: {e}")
@@ -79,13 +64,15 @@ def hero_by_name(hero_name, hero_data):
 
 def print_hero_info(hero):
     if hero:
-        print(f"Информация о герое {hero['localized_name']}:")
-        print(f"Полномочия героя: {hero['roles']}")
-        print(f"Основной аттрибут: {hero['primary_attr']}")
-        print(f"Тип атаки: {hero['attack_type']}")
+        hero_name = f"Информация о герое {hero['localized_name']}:"
+        hero_roles = f"Полномочия героя: {hero['roles']}"
+        hero_main_attr = f"Основной аттрибут: {hero['primary_attr']}"
+        hero_type_attack = f"Тип атаки: {hero['attack_type']}"
+        print(hero_name, hero_roles, hero_main_attr, hero_type_attack, sep = "\n")
         print()
 
 def main():
+    heroes_data = hero_info()
     print("Программа выдаёт 2 лучших и худших героев против введённого, их статистику матчей, а также некоторые характеристики введённого героя.")
     while True:
         hero_name = input("Введите имя героя, чтобы узнать его статистику и характеристики (для выхода введите 'exit'): ")
@@ -93,8 +80,8 @@ def main():
             break
         hero = hero_by_name(hero_name, heroes_data)
         if hero:
-            versus_heroes = get_hero_versus(hero['id'], heroes_data)
             print_hero_info(hero)
+            versus_heroes = get_hero_versus(hero['id'], heroes_data)
         else:
             print("Герой не найден.")
 
