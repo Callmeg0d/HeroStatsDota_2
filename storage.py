e8c1c20e2289f5ff62d50bc7b48e7ece7f5f1708
Aleksey Ryabchikov <cyberxx@yandex.ru>
2024-06-27 16:41:57 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,15 +1,12 @@
-import requests
 class HeroesStorage:
     def __init__(self):
         self.heroes_data = []
 
-    def set(self, url):
-        response = requests.get(url)
-        self.heroes_data = response.json()
+    def set(self, heroes_data):
+        self.heroes_data = heroes_data
 
     def get(self):
-        self.set("https://api.opendota.com/api/heroes")
         return self.heroes_data
 
+
 heroes_storage = HeroesStorage()
-heroes_data = heroes_storage.get()
