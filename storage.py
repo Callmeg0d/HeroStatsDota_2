97b532c17026bddaf3ad03b7779ad6cf35fa4c6d
Callmegod <125900081+Callmeg0d@users.noreply.github.com>
2024-06-27 13:35:50 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -3,14 +3,13 @@ class HeroesStorage:
     def __init__(self):
         self.heroes_data = []
 
-    @classmethod
     def set(self, url):
         response = requests.get(url)
         self.heroes_data = response.json()
 
-    @classmethod
     def get(self):
         self.set("https://api.opendota.com/api/heroes")
         return self.heroes_data
 
 heroes_storage = HeroesStorage()
+heroes_data = heroes_storage.get()
